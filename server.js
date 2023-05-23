f52dd3665a3186148f263b56e0b32019bfb8526f
Marko Knöbl <marko.kn@gmail.com>
2023-05-23 20:25:21 +0200

    basic random number generator

--- server.js
+++ server.js
@@ -6,4 +6,9 @@ app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
+app.get("/random", (req, res) => {
+  console.log(req.query);
+  res.send(String(Math.floor(Math.random() * 10)));
+});
+
 app.listen(3000);
